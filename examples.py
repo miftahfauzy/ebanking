cc596d8fed1a05141db43e9bfda8b3529a80febb
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-13 21:58:54 +0700

    last update

---
+++ examples.py
@@ -0,0 +1,33 @@
+from gino import Gino
+
+db = Gino()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer(),primary_key=True)
+    nickname = db.Column(db.Unicode(),default='noname')
+
+
+async def main():
+    async with db.with_bind('asyncpg://localhost/gino'):
+
+        # Create tables
+        await db.gino.create_all()
+
+        # Create object, `id` is assigned by database
+        u1 = await User.create(nickname='fantix')
+        print(u1.id, u1.nickname) # 1 fantix
+
+        # Execute complex statement and return command status
+        status, result = await User.update.values(
+            nickname='No.' +db.cast(User.id, db.Unicode),
+        ).where(
+            User.id > 10,
+        ).gino.status()
+        print(status) # UPDATE 8
+
+        # Iterate over the results of a large query in a transaction
+        async with db.transaction():
+            async for u in User.query.order_by(User.id).gino.iterate():
+                print(u.id, u.nickname)
