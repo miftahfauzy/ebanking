984fdb20f242fc9194c75445a36b5dc28d455f39
Miftah Fauzy <miftah.fauzy@gmail.com>
2019-09-13 14:37:59 +0700

    modified:   initdb.py

--- initdb.py
+++ initdb.py
@@ -3,6 +3,7 @@ from gino import Gino
 
 db = Gino()
 
+
 class Customer(db.Model):
     __tablename__ = "customer"
 
@@ -19,6 +20,7 @@ class Customer(db.Model):
     insert_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     update_at = db.Column(db.DateTime, nullable=True)
 
+
 class Branches(db.Model):
     __tablename__ = "branches"
     branch_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -31,6 +33,7 @@ class Branches(db.Model):
     insert_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     update_at = db.Column(db.DateTime, nullable=True)
 
+
 class AccountType(db.Model):
     __tablename__ = "account_type"
 
@@ -38,6 +41,20 @@ class AccountType(db.Model):
     minimum_balance_restriction = db.Column(db.Number, nullable=True)
     insert_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     update_at = db.Column(db.DateTime, nullable=True)
+
+
+class Accounts(db.Model):
+    __tablename__ = "accounts"
+    account_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    account_balance = db.Column(db.Numeric, nullable=True)
+    street_address = db.Column(db.String(50), nullable=True)
+    city = db.Column(db.String(25), nullable=True)
+    state = db.Column(db.String(3), nullable=True)
+    zipcode = db.Column(db.Integer, nullable=True)
+    phone_number = db.Column(db.String(12), nullable=True)
+    insert_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    update_at = db.Column(db.DateTime, nullable=True)
+
 
 async def migrate():
     await db.set_bind('postgres://miftah:fonez@localhost/ebanking')
