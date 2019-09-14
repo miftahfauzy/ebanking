7cd0833235b4c6f13f030d529de7149a1edd969b
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-14 17:06:30 +0700

    last update

--- customer_list.py
+++ customer_list.py
@@ -2,35 +2,34 @@ import asyncio
 import asyncpg
 from initdb import db, Customers
 
-async with db.with_bind('asyncpg://miftah:fonez@localhost/ebanking'):
-
 
 class CustomerService:
-    async def customer_list():
+    async with db.with_bind('asyncpg://miftah:fonez@localhost/ebanking'):
+        async def customer_list():
 
-        async with db.transaction():
+            async with db.transaction():
 
-            customers = []
+                customers = []
 
-            # Iterate over the results of a large query in a transaction
-            async for customer in Customers.query.order_by(Customers.customer_id).gino.iterate():
-                # print(customers.customer_id, customers.first_name, customers.last_name, customers.date_of_birth)
-                dict_customer = {
-                    "customer_id": customer.customer_id,
-                    "first_name": customer.first_name,
-                    "last_name": customer.last_name,
-                    "date_of_birth": customer.date_of_birth,
-                    "street_address": customer.street_address,
-                    "city": customer.city,
-                    "state": customer.state,
-                    "zipcode": customer.zipcode,
-                    "email": customer.email,
-                    "gender": customer.gender,
-                    "insert_at": customer.insert_at,
-                    "update_at": customer.update_at
-                }
-                customers.append(dict_customer)
-            return customers
+                # Iterate over the results of a large query in a transaction
+                async for customer in Customers.query.order_by(Customers.customer_id).gino.iterate():
+                    # print(customers.customer_id, customers.first_name, customers.last_name, customers.date_of_birth)
+                    dict_customer = {
+                        "customer_id": customer.customer_id,
+                        "first_name": customer.first_name,
+                        "last_name": customer.last_name,
+                        "date_of_birth": customer.date_of_birth,
+                        "street_address": customer.street_address,
+                        "city": customer.city,
+                        "state": customer.state,
+                        "zipcode": customer.zipcode,
+                        "email": customer.email,
+                        "gender": customer.gender,
+                        "insert_at": customer.insert_at,
+                        "update_at": customer.update_at
+                    }
+                    customers.append(dict_customer)
+                return customers
 
 
     async def get_customer(_customer_id):
