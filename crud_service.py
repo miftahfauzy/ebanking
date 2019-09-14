fb7e4829f006961809599ec771bbba64d1aa7592
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-14 09:22:54 +0700

    last update

--- crud_service.py
+++ crud_service.py
@@ -21,16 +21,16 @@ db = Gino()
 
 
 class CustomerService:
-    async def object_as_dict(obj):
+    def object_as_dict(obj):
         return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
 
-    async def customer_list():
+    def customer_list():
         try:
-            async with db.with_bind('postgres://miftah:fonez@localhost/ebanking'):
+            with db.with_bind('postgres://miftah:fonez@localhost/ebanking'):
 
-                async with db.transaction():
+                with db.transaction():
                     customers = []
-                    async for customer in Customers.query.order_by(Customers.customer_id).gino.iterate():
+                    for customer in Customers.query.order_by(Customers.customer_id).gino.iterate():
                         dict_customer = {
                             "customer_id": customer.customer_id,
                             "first_name": customer.first_name,
@@ -45,10 +45,10 @@ class CustomerService:
                             "insert_at": customer.insert_at,
                             "update_at": customer.update_at
                         }
-                        await customers.append(dict_customer)
+                        customers.append(dict_customer)
             return customers
         except ValueError:
             return str(ValueError)
 
 
-asyncio.get_event_loop().run_until_complete(CustomerService.customer_list())
+CustomerService.customer_list()
