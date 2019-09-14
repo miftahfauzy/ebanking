1d7139b539afea5bb05b869cb24f9322d29ecff8
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-14 15:30:45 +0700

    last update

---
+++ customer_list.py
@@ -0,0 +1,17 @@
+import asyncio
+from gino import Gino
+from initdb import Customers
+
+db = Gino()
+
+
+async def migrate():
+    async with db.with_bind('asyncpg://miftah:fonez@localhost/ebanking'):
+
+        # Iterate over the results of a large query in a transaction
+        async with db.transaction():
+            async for customers in Customers.query.order_by(Customers.id).gino.iterate():
+                print(customers.id, customers.first_name, customers.last_name, customers.date_of_birth)
+
+
+asyncio.get_event_loop().run_until_complete(migrate())
