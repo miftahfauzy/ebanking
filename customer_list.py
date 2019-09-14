5a1eae31b76bde66575291514f4050d3ee7a9d19
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-14 15:34:50 +0700

    last update

--- customer_list.py
+++ customer_list.py
@@ -10,7 +10,7 @@ async def migrate():
 
         # Iterate over the results of a large query in a transaction
         async with db.transaction():
-            async for customers in Customers.query.order_by(Customers.id).gino.iterate():
+            async for customers in Customers.query.order_by(Customers.customer_id).gino.iterate():
                 print(customers.customer_id, customers.first_name, customers.last_name, customers.date_of_birth)
 
 
