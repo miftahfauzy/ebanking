3fbc50537f80a3116ba43b6ff3155cad9d4df23c
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-14 22:30:52 +0700

    last update

--- customer_list.py
+++ customer_list.py
@@ -31,27 +31,27 @@ class CustomerService:
         return customers
 
     async def get_customer(_customer_id):
-        async with db.with_bind("postgresql://miftah:fonez@localhost/ebanking"):
+        await db.set_bind("postgresql://miftah:fonez@localhost/ebanking")
 
-            customer = await Customers.get(_customer_id)
-            print(customer.customer_id, customer.first_name)
+        customer = await Customers.get(_customer_id)
+        # print(customer.customer_id, customer.first_name)
 
-            dict_customer = {
-                "customer_id": customer.customer_id,
-                "first_name": customer.first_name,
-                "last_name": customer.last_name,
-                "date_of_birth": customer.date_of_birth,
-                "street_address": customer.street_address,
-                "city": customer.city,
-                "state": customer.state,
-                "zipcode": customer.zipcode,
-                "email": customer.email,
-                "gender": customer.gender,
-                "insert_at": customer.insert_at,
-                "update_at": customer.update_at,
-            }
-            await db.pop_bind().close()
-            return dict_customer
+        dict_customer = {
+            "customer_id": customer.customer_id,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "date_of_birth": customer.date_of_birth,
+            "street_address": customer.street_address,
+            "city": customer.city,
+            "state": customer.state,
+            "zipcode": customer.zipcode,
+            "email": customer.email,
+            "gender": customer.gender,
+            "insert_at": customer.insert_at,
+            "update_at": customer.update_at,
+        }
+        await db.pop_bind().close()
+        return dict_customer
 
 
 # customers_dict = asyncio.get_event_loop().run_until_complete(CustomerService.customer_list())
