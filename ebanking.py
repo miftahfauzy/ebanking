d98f3ea0565ad325dd483933b250d4bb3a0826f7
miftahfauzy <miftah.fauzy@gmail.com>
2019-09-16 11:21:36 +0700

    last update

--- ebanking.py
+++ ebanking.py
@@ -11,8 +11,8 @@ router = api.router
 router.add_route("/customer/list", CustomerResource.list_customer, method="GET")
 router.add_route("/customer/{customer_id}", CustomerResource.get_customer, method="GET")
 router.add_route("/customer/create", CustomerResource.create_customer, method="POST")
-router.add_route("/customer/update/{customer_id}", CustomerResource.update_product, method="PUT")
-router.add_route("/customer/delete/{customer_id}", CustomerResource.delete_product, method="DELETE")
+router.add_route("/customer/update/{customer_id}", CustomerResource.delete_customer, method="PUT")
+router.add_route("/customer/delete/{customer_id}", CustomerResource.delete_customer, method="DELETE")
 
 
 api.run(port=8000)
